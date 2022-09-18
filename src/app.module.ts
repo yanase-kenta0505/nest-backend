5f214eb04e099e97dd975c2ff8ec4bb48d51b93a
yanasekenta <ynsknt0505@gmail.com>
2022-09-19 08:57:38 +0900

    graphqlのオプションを設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 @Module({
-  imports: [],
+  /**
+   * @link https://docs.nestjs.com/graphql/migration-guide#federation
+   */
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql'
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
